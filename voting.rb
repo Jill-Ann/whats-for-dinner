6cd27e76d2d3d74bed285b8f6d5e2f025a0bb9ba
Jill <jillannkay@gmail.com>
2020-03-30 20:49:40 +0200

    first commit

---
+++ voting.rb
@@ -0,0 +1,36 @@
+require 'yaml/store'
+
+require 'sinatra'
+
+
+get '/' do
+  @title = 'Cast your vote!'
+  erb :index
+end
+
+
+Choices = {
+  'HAM' => 'Hamburger',
+  'PIZ' => 'Pizza',
+  'CUR' => 'Curry',
+  'NOO' => 'Noodles',
+}
+
+post '/cast' do
+  @title = 'Thanks for casting your vote!'
+  @vote  = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
+  erb :cast
+end
+
+get '/results' do
+  @title = 'Results so far:'
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
+  erb :results
+end
